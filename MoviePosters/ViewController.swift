71f1096495070fd163ec6d467a1ce42b5bf1be3a
Isaiah Cuzzupe <upmatter@hotmail.com>
2020-07-27 18:58:12 +1000

    Initial Commit

---
+++ MoviePosters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePosters
+//
+//  Created by Isaiah Cuzzupe on 27/7/20.
+//  Copyright © 2020 s3743803_s3728738_s3541003. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoviePostersTests/MoviePostersTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoviePostersTests.swift
+//  MoviePostersTests
+//
+//  Created by Isaiah Cuzzupe on 27/7/20.
+//  Copyright © 2020 s3743803_s3728738_s3541003. All rights reserved.
+//
+
+import XCTest
+@testable import MoviePosters
+
+class MoviePostersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
